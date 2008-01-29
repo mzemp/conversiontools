b94105b117c58749f4839b44415506919a21a72b
Marcel Zemp <marcelzemp@gmail.com>
2008-01-29 01:23:42 +0000

    Added array field name possibility to ts2silo

--- tipsy_xdr_2_silo.c
+++ tipsy_xdr_2_silo.c
@@ -12,11 +12,14 @@
 #include <silo.h>
 #include <iof.h>
 
+#define NARRAYMAX 20
+
 void usage(void);
 
 int main(int argc, char **argv) {
 
     int i, j;
+    int getmatch = 0;
     int positionprecision = 0;
     int arrayfile = 0;
     int nvar = 8;
@@ -41,7 +44,11 @@ int main(int argc, char **argv) {
     const char *star_defs[] = {"coord(<star/star_pos>)[0]","coord(<star/star_pos>)[1]","coord(<star/star_pos>)[2]",
 			  "dot(<star/star_vel>,{1,0,0})","dot(<star/star_vel>,{0,1,0})","dot(<star/star_vel>,{0,0,1})",
 			  "polar_radius(<star/star_pos>)","magnitude(<star/star_vel>)"};
-    char outputname[100], arrayfilename[100], arrayname[100];
+    char outputname[30], arrayfilename[30], arrayname[30];
+    char iname[NARRAYMAX][30];
+    char fname[NARRAYMAX][30];
+    char dname[NARRAYMAX][30];
+    char reservednames[10][30];
     float **posf = NULL;
     double **posd = NULL;;
     float **vel;
@@ -67,18 +74,39 @@ int main(int argc, char **argv) {
     DBfile *dbfile = NULL;
     FILE *file;
     XDR xdrs;
-
+    /*
+    ** Set some default values
+    */
+    for (i = 0; i < NARRAYMAX; i++) {
+	sprintf(iname[i],"int%d",i+1);
+	sprintf(fname[i],"float%d",i+1);
+	sprintf(dname[i],"double%d",i+1);
+	}
+    strcpy(reservednames[0],"pos");
+    strcpy(reservednames[1],"vel");
+    strcpy(reservednames[2],"mass");
+    strcpy(reservednames[3],"hsmooth");
+    strcpy(reservednames[4],"eps");
+    strcpy(reservednames[5],"phi");
+    strcpy(reservednames[6],"metals");
+    strcpy(reservednames[7],"rho");
+    strcpy(reservednames[8],"temp");
+    strcpy(reservednames[9],"tform");
+    /*
+    ** Get parameters
+    */
     i = 1;
     while (i < argc) {
         if (strcmp(argv[i],"-spp") == 0) {
             positionprecision = 0;
             i++;
+	    continue;
             }
         else if (strcmp(argv[i],"-dpp") == 0) {
             positionprecision = 1;
             i++;
+	    continue;
             }
-
 	else if (strcmp(argv[i],"-o") == 0) {
             i++;
             if (i >= argc) {
@@ -86,6 +114,7 @@ int main(int argc, char **argv) {
                 }
             strcpy(outputname,argv[i]);
             i++;
+	    continue;
             }
 	else if (strcmp(argv[i],"-array") == 0) {
             i++;
@@ -95,14 +124,117 @@ int main(int argc, char **argv) {
 	    arrayfile = 1;
             strcpy(arrayfilename,argv[i]);
             i++;
+	    continue;
             }
-	else if ((strcmp(argv[i],"-h") == 0) || (strcmp(argv[i],"-help") == 0)) {
+	getmatch = 0;
+	for (j = 0; j < NARRAYMAX; j++) {
+	    sprintf(arrayname,"-i%d",j+1);
+	    if (strcmp(argv[i],arrayname) == 0) {
+		getmatch = 1;
+		i++;
+		if (i >= argc) {
+		    usage();
+		    }
+		strcpy(iname[j],argv[i]);
+		i++;
+		}
+	    }
+	if (getmatch == 1) {
+	    continue;
+	    }
+	for (j = 0; j < NARRAYMAX; j++) {
+	    sprintf(arrayname,"-f%d",j+1);
+	    if (strcmp(argv[i],arrayname) == 0) {
+		getmatch = 1;
+		i++;
+		if (i >= argc) {
+		    usage();
+		    }
+		strcpy(fname[j],argv[i]);
+		i++;
+		}
+	    }
+	if (getmatch == 1) {
+	    continue;
+	    }
+	for (j = 0; j < NARRAYMAX; j++) {
+	    sprintf(arrayname,"-d%d",j+1);
+	    if (strcmp(argv[i],arrayname) == 0) {
+		getmatch = 1;
+		i++;
+		if (i >= argc) {
+		    usage();
+		    }
+		strcpy(dname[j],argv[i]);
+		i++;
+		}
+	    }
+	if (getmatch == 1) {
+	    continue;
+	    }
+	if ((strcmp(argv[i],"-h") == 0) || (strcmp(argv[i],"-help") == 0)) {
             usage();
             }
         else {
             usage();
             }
         }
+    /*
+    ** Check if some reserved or double names were chosen
+    */
+    for (i = 0; i < 10; i++) {
+	for (j = 0; j < NARRAYMAX; j++) {
+	    if (strcmp(iname[j],reservednames[i]) == 0) {
+		fprintf(stderr,"Chosen name for i%d array field matches a reserved name (%s)!\n",j+1,reservednames[i]);
+		fprintf(stderr,"Make a different choice.\n");
+		usage();
+		}
+	    if (strcmp(fname[j],reservednames[i]) == 0) {
+		fprintf(stderr,"Chosen name for f%d array field matches a reserved name (%s)!\n",j+1,reservednames[i]);
+		fprintf(stderr,"Make a different choice.\n");
+		usage();
+		}
+	    if (strcmp(dname[j],reservednames[i]) == 0) {
+		fprintf(stderr,"Chosen name for d%d array field matches a reserved name (%s)!\n",j+1,reservednames[i]);
+		fprintf(stderr,"Make a different choice.\n");
+		usage();
+		}
+	    }
+	}
+    for (i = 0; i < NARRAYMAX; i++) {
+	for (j = 0; j < NARRAYMAX; j++) {
+	    if ((strcmp(iname[j],iname[i]) == 0) && (i != j)) {
+		fprintf(stderr,"You chose the same name for the i%d and i%d array fields (%s)!\n",i+1,j+1,iname[i]);
+		fprintf(stderr,"Make a different choice.\n");
+		usage();
+		}
+	    if (strcmp(iname[j],fname[i]) == 0) {
+		fprintf(stderr,"You chose the same name for the i%d and f%d array fields (%s)!\n",i+1,j+1,iname[i]);
+		fprintf(stderr,"Make a different choice.\n");
+		usage();
+		}
+	    if (strcmp(iname[j],dname[i]) == 0) {
+		fprintf(stderr,"You chose the same name for the i%d and d%d array fields (%s)!\n",i+1,j+1,iname[i]);
+		fprintf(stderr,"Make a different choice.\n");
+		usage();
+		}
+	    if ((strcmp(fname[j],fname[i]) == 0) && (i != j)) {
+		fprintf(stderr,"You chose the same name for the f%d and f%d array fields (%s)!\n",i+1,j+1,fname[i]);
+		fprintf(stderr,"Make a different choice.\n");
+		usage();
+		}
+	    if (strcmp(fname[j],dname[i]) == 0) {
+		fprintf(stderr,"You chose the same name for the f%d and d%d array fields (%s)!\n",i+1,j+1,fname[i]);
+		fprintf(stderr,"Make a different choice.\n");
+		usage();
+		}
+	    if ((strcmp(dname[j],dname[i]) == 0) && (i != j)) {
+		fprintf(stderr,"You chose the same name for the d%d and d%d array fields (%s)!\n",i+1,j+1,fname[i]);
+		fprintf(stderr,"Make a different choice.\n");
+		usage();
+		}
+	    }
+	}
     /*
     ** Create silo file
     */
@@ -190,7 +322,7 @@ int main(int argc, char **argv) {
 		mass[i] = gp.mass;
 		eps[i] = gp.hsmooth;
 		phi[i] = gp.phi;
-		rho[i] = gp.metals;
+		rho[i] = gp.rho;
 		temp[i] = gp.temp;
 		metals[i] = gp.metals;
 		}
@@ -203,7 +335,7 @@ int main(int argc, char **argv) {
 		mass[i] = gpdpp.mass;
 		eps[i] = gpdpp.hsmooth;
 		phi[i] = gpdpp.phi;
-		rho[i] = gpdpp.metals;
+		rho[i] = gpdpp.rho;
 		temp[i] = gpdpp.temp;
 		metals[i] = gpdpp.metals;
 		}
@@ -326,7 +458,7 @@ int main(int argc, char **argv) {
 		mass[i] = sp.mass;
 		eps[i] = sp.eps;
 		phi[i] = sp.phi;
-		metals[i] = sp.phi;
+		metals[i] = sp.metals;
 		tform[i] = sp.tform;
 		}
 	    else if (positionprecision == 1) {
@@ -338,7 +470,7 @@ int main(int argc, char **argv) {
 		mass[i] = spdpp.mass;
 		eps[i] = spdpp.eps;
 		phi[i] = spdpp.phi;
-		metals[i] = spdpp.phi;
+		metals[i] = spdpp.metals;
 		tform[i] = spdpp.tform;
 		}
 	    }
@@ -355,7 +487,7 @@ int main(int argc, char **argv) {
 	DBPutPointvar1(dbfile,"star_eps","star_pos",eps,th.nstar,DB_FLOAT,NULL);
 	DBPutPointvar1(dbfile,"star_phi","star_pos",phi,th.nstar,DB_FLOAT,NULL);
 	DBPutPointvar1(dbfile,"star_metals","star_pos",metals,th.nstar,DB_FLOAT,NULL);
-	DBPutPointvar1(dbfile,"star_tfrom","star_pos",tform,th.nstar,DB_FLOAT,NULL);
+	DBPutPointvar1(dbfile,"star_tform","star_pos",tform,th.nstar,DB_FLOAT,NULL);
 	DBPutDefvars(dbfile,"variables",nvar,star_names,types,star_defs,NULL);
 	DBSetDir(dbfile,"/");
 	}
@@ -382,6 +514,10 @@ int main(int argc, char **argv) {
 	xdrstdio_create(&xdrs,file,XDR_DECODE);
 	read_array_header(&xdrs,&ah);
 	assert(ah.N[0] == th.ntotal);
+	if (ah.N[1] > NARRAYMAX || ah.N[2] > NARRAYMAX || ah.N[3] > NARRAYMAX) {
+	    fprintf(stderr,"Too many array fields! Recompile with higher NARRAYMAX value.\n");
+	    exit(1);
+	    }
 	allocate_array_particle(&ah,&ap);
 	if (ah.N[1] > 0) {
 	    ia = realloc(ia,ah.N[1]*sizeof(int *));
@@ -431,15 +567,15 @@ int main(int argc, char **argv) {
 		}
 	    DBSetDir(dbfile,"/gas");
 		for (j = 0; j < ah.N[1]; j++) {
-		    sprintf(arrayname,"gas_i%d",j+1);
+		    sprintf(arrayname,"gas_%s",iname[j]);
 		    DBPutPointvar1(dbfile,arrayname,"gas_pos",(float *)ia[j],th.ngas,DB_INT,NULL);
 		    }
 		for (j = 0; j < ah.N[2]; j++) {
-		    sprintf(arrayname,"gas_f%d",j+1);
+		    sprintf(arrayname,"gas_%s",fname[j]);
 		    DBPutPointvar1(dbfile,arrayname,"gas_pos",fa[j],th.ngas,DB_FLOAT,NULL);
 		    }
 		for (j = 0; j < ah.N[3]; j++) {
-		    sprintf(arrayname,"gas_d%d",j+1);
+		    sprintf(arrayname,"gas_%s",dname[j]);
 		    DBPutPointvar1(dbfile,arrayname,"gas_pos",(float *)da[j],th.ngas,DB_DOUBLE,NULL);
 		    }
 	    DBSetDir(dbfile,"/");
@@ -471,15 +607,15 @@ int main(int argc, char **argv) {
 		}
 	    DBSetDir(dbfile,"/dark");
 		for (j = 0; j < ah.N[1]; j++) {
-		    sprintf(arrayname,"dark_i%d",j+1);
+		    sprintf(arrayname,"dark_%s",iname[j]);
 		    DBPutPointvar1(dbfile,arrayname,"dark_pos",(float *)ia[j],th.ndark,DB_INT,NULL);
 		    }
 		for (j = 0; j < ah.N[2]; j++) {
-		    sprintf(arrayname,"dark_f%d",j+1);
+		    sprintf(arrayname,"dark_%s",fname[j]);
 		    DBPutPointvar1(dbfile,arrayname,"dark_pos",fa[j],th.ndark,DB_FLOAT,NULL);
 		    }
 		for (j = 0; j < ah.N[3]; j++) {
-		    sprintf(arrayname,"dark_d%d",j+1);
+		    sprintf(arrayname,"dark_%s",dname[j]);
 		    DBPutPointvar1(dbfile,arrayname,"dark_pos",(float *)da[j],th.ndark,DB_DOUBLE,NULL);
 		    }
 	    DBSetDir(dbfile,"/");
@@ -511,15 +647,15 @@ int main(int argc, char **argv) {
 		}
 	    DBSetDir(dbfile,"/star");
 		for (j = 0; j < ah.N[1]; j++) {
-		    sprintf(arrayname,"star_i%d",j+1);
+		    sprintf(arrayname,"star_%s",iname[j]);
 		    DBPutPointvar1(dbfile,arrayname,"star_pos",(float *)ia[j],th.nstar,DB_INT,NULL);
 		    }
 		for (j = 0; j < ah.N[2]; j++) {
-		    sprintf(arrayname,"star_f%d",j+1);
+		    sprintf(arrayname,"star_%s",fname[j]);
 		    DBPutPointvar1(dbfile,arrayname,"star_pos",fa[j],th.nstar,DB_FLOAT,NULL);
 		    }
 		for (j = 0; j < ah.N[3]; j++) {
-		    sprintf(arrayname,"star_d%d",j+1);
+		    sprintf(arrayname,"star_%s",dname[j]);
 		    DBPutPointvar1(dbfile,arrayname,"star_pos",(float *)da[j],th.nstar,DB_DOUBLE,NULL);
 		    }
 	    DBSetDir(dbfile,"/");
@@ -546,11 +682,12 @@ void usage(void) {
     fprintf(stderr,"\n");
     fprintf(stderr,"Please specify the following parameters:\n");
     fprintf(stderr,"\n");
-    fprintf(stderr,"-spp          : set this flag if input and output file have single precision positions (default)\n");
-    fprintf(stderr,"-dpp          : set this flag if input and output file have double precision positions\n");
-    fprintf(stderr,"-o <name>     : output file in silo format\n");
-    fprintf(stderr,"-array <name> : array file in array standard binary format\n");
-    fprintf(stderr,"< <name>      : input file in tipsy standard binary format\n");
+    fprintf(stderr,"-spp           : set this flag if input and output file have single precision positions (default)\n");
+    fprintf(stderr,"-dpp           : set this flag if input and output file have double precision positions\n");
+    fprintf(stderr,"-<a><i> <name> : name of array field (optional), <a> array type (i,f or d), <i> array index\n");
+    fprintf(stderr,"-o <name>      : output file in silo format\n");
+    fprintf(stderr,"-array <name>  : array file in array standard binary format\n");
+    fprintf(stderr,"< <name>       : input file in tipsy standard binary format\n");
     fprintf(stderr,"\n");
     exit(1);
     }
